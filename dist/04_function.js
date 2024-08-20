80a7cdaffd4416c9634a7f3686b0ffc0365e4afc
Abhishek83gupta <abhi12345678gupta@gmail.com>
2024-08-20 22:56:36 +0530

    function done

---
+++ dist/04_function.js
@@ -0,0 +1,11 @@
+"use strict";
+// return type // void -> no data
+function birthdayWish(name, age) {
+    console.log(` Happy Birthday ${name} you have completed your ${age} years `);
+}
+birthdayWish("Abhishek", 21);
+function add(num1, num2) {
+    return num1 + num2;
+}
+let result = add(65, 56);
+console.log(result);
---
+++ src/04_function.ts
@@ -0,0 +1,16 @@
+                                                 // return type // void -> no data
+function birthdayWish(name : string ,age : number): void {
+  console.log(` Happy Birthday ${name} you have completed your ${age} years `)
+}
+
+
+birthdayWish("Abhishek", 21);
+
+
+
+function add(num1:number , num2:number):number{
+  return num1+num2;
+}
+
+let result = add(65,56)
+console.log(result);
