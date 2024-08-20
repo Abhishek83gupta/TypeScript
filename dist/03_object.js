64dcbdb7b5388990a8e9dcde6fc5a9aff0e64504
Abhishek83gupta <abhi12345678gupta@gmail.com>
2024-08-20 22:56:02 +0530

    done

---
+++ dist/03_object.js
@@ -0,0 +1,9 @@
+"use strict";
+let student = {
+    name: "Abhishek",
+    address: "Mumbai",
+    phone: 234235234,
+    isActive: true
+};
+console.log(student.name);
+console.log(student["address"]);
---
+++ src/03_object.ts
@@ -0,0 +1,14 @@
+let student : {
+  name:string,
+  address:string,
+  phone:number,
+  isActive:true
+} = {
+  name:"Abhishek",
+  address : "Mumbai",
+  phone : 234235234,
+  isActive:true
+}
+
+console.log(student.name)
+console.log(student["address"]);
