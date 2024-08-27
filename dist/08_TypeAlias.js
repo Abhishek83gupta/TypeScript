067226e5007e3aba4baa4593f2214ee3660dd7c5
Abhishek83gupta <abhi12345678gupta@gmail.com>
2024-08-27 23:53:38 +0530

    done

---
+++ dist/08_TypeAlias.js
@@ -0,0 +1,19 @@
+"use strict";
+// Type Alias
+// Create Temp name of type 
+// Temporary name
+// Abhishek --> (Abhi)
+let homeAddress = "Thane";
+console.log(typeof homeAddress); // String
+let pass;
+pass = 3445543;
+pass = "abc2323";
+console.log(typeof pass);
+let order1;
+order1 = {
+    title: "This is title of order",
+    price: 234455,
+    status: "This is Status"
+};
+// String literals
+let myName;
